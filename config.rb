e11c452b01a4b9ed74f9cc40ec4f73b79b7d6b1d
Luke Spragg <luke@spragg.me>
2015-02-05 15:00:35 -0500

    Update build and deployment configurations

--- config.rb
+++ config.rb
@@ -10,19 +10,21 @@ set :markdown_engine, :redcarpet
 
 set :markdown, :fenced_code_blocks => true, :smartypants => true, :disable_indented_code_blocks => true, :prettify => true, :tables => true, :with_toc_data => true, :no_intra_emphasis => true
 
+# Activate automatic page reloading
+activate :livereload
+
 # Activate the syntax highlighter
 activate :syntax
 
-# This is needed for Github pages, since they're hosted on a subdomain
+# This is needed for GitHub pages, since they're hosted on a subdomain
 activate :relative_assets
 set :relative_links, true
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
+  # Minify static assets on build
   activate :minify_css
-
-  # Minify Javascript on build
+  activate :minify_html
   activate :minify_javascript
 
   # Enable cache buster
@@ -34,4 +36,3 @@ configure :build do
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
 end
-
